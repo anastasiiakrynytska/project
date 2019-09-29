d095c5bb61ed4f83d62468f087944b3082ad02b0
anastasiiakrynytska <anastasiiakrynytska@gmail.coms>
2019-09-29 13:32:57 +0300

    add handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,43 @@
-node_modules
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
+const connect = require('gulp-connect');
+gulpSass.compiler = require('node-sass');
+
+gulp.task('sass', () => {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(gulpSass({ outputStyle: 'compressed' }).on('error', gulpSass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('template', () => {
+    options = {
+        ignorePartials: true,
+        batch: ['./templates/partials'],
+        helpers: {
+            ifvalue: function (conditional, options) {
+                if (options.hash.value === conditional) {
+                    return options.fn(this)
+                } else {
+                    return options.inverse(this);
+                }
+            }
+        }
+    }
+    return gulp.src('templates/*.hbs')
+        .pipe(handlebars(null, options))
+        .pipe(rename((path) => {
+            path.extname = '.html';
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', () => {
+    connect.server();
+    gulp.watch('./scss/**/*.scss', gulp.series(['sass']));
+    gulp.watch('./templates/**/*.hbs', gulp.series(['template']));
+});
